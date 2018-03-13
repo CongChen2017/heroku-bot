81ed928577ee4ea6cca4f6433070781018d04509
congchen2017 <cong-chen@northwestern.edu>
2018-03-12 19:48:51 -0500

    change code

--- ChatterBot.py
+++ ChatterBot.py
@@ -36,7 +36,7 @@ api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
 # Create a function that tweets
 def TweetOut(tweet_number):
     api.update_status(
-        "Can't stop. Won't stop. Chatting! This is Tweet #%s!" %
+        "Can't stop. Won't stop. Chatting to You! This is Tweet #%s!" %
         tweet_number)
 
 
